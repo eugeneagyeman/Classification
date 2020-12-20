31950ae640cc4cb70d15e8bc1696de12fbc1c344
Emi <tkj1g18@soton.ac.uk>
2020-12-20 13:27:03 +0000

    Cleaned code and removed main() method. Code now can be run externally as an instanced class

--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -5,11 +5,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
 
+/**
+ * Simple helper class used to assist in writing results of classification algorithms to text files.
+ *
+ * Written by Emily James, Dec 2020
+ */
 public class Writer {
 
     FileWriter writer;
     String path;
 
+    /**
+     * Automatically creates a file in the working directory under the name
+     * {@code: run<runNo>.txt}.
+     * @param runNo
+     */
     public Writer(int runNo) {
         path = "run" + runNo + ".txt";
         try {
@@ -28,10 +38,20 @@ public class Writer {
     }
 
     public int writeResult(FileObject imageFile, String classifier) {
-        return writeStr(imageFile.getName().getBaseName()+" "+classifier+"\n");
+        return writeResult(imageFile.getName().getBaseName(), classifier);
     }
 
-    public int writeResults(Map<FileObject, String> results) {
+    public int writeResult(String imageFileName, String classifier) {
+        return writeStr(imageFileName+" "+classifier+"\n");
+    }
+
+    /**
+     * A variant to {@code writeResults(..)} which uses FileObjects instead
+     * of Strings.
+     * @param results
+     * @return
+     */
+    public int writeFOResults(Map<FileObject, String> results) {
         final int[] r = {1};
         results.forEach( (fo, c) -> {
             r[0] &=
@@ -41,7 +61,18 @@ public class Writer {
         return r[0]-1;
     }
 
-    public int writeStr(String str) {
+    public int writeResults(Map<String, String> results) {
+        final int[] r = {1};
+        results.forEach( (fo, c) -> {
+            r[0] &=
+                    writeResult(fo, c);
+        });
+
+        return r[0]-1;
+    }
+
+
+    private int writeStr(String str) {
         try {
             writer.write(str);
             return 0;
