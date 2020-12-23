a80dd58fe72ca1aebf161fd988b0b6f8e795eed7
Emi <tkj1g18@soton.ac.uk>
2020-12-23 12:36:48 +0000

    Removed main method in Writer class, added helper functions to abstract and standardise output matching the coursework specification

--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -1,20 +1,86 @@
+import org.apache.commons.vfs2.FileObject;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
+/**
+ * Simple helper class used to assist in writing results of classification algorithms to text files.
+ *
+ * Written by Emily James, Dec 2020
+ */
 public class Writer {
 
     FileWriter writer;
+    String path;
 
-    public Writer(String path) {
-        File f = new File(path);
+    /**
+     * Automatically creates a file in the working directory under the name
+     * <tt>run[runNo].txt</tt>.
+     * @param runNo
+     */
+    public Writer(int runNo) {
+        path = "run" + runNo + ".txt";
         try {
             writer = new FileWriter(path);
         } catch (IOException e) {
         }
     }
 
-    public int write(String str) {
+    public Writer(String fn) {
+        path = fn;
+        try {
+            writer = new FileWriter(path);
+        } catch (IOException e) {
+        }
+    }
+
+    public int reopenFile() {
+        try {
+            writer = new FileWriter(path);
+            return 0;
+        } catch (IOException e) {
+            return -1;
+        }
+    }
+
+    public int writeResult(FileObject imageFile, String classifier) {
+        return writeResult(imageFile.getName().getBaseName(), classifier);
+    }
+
+    public int writeResult(String imageFileName, String classifier) {
+        return writeStr(imageFileName+" "+classifier+"\n");
+    }
+
+    /**
+     * A variant to {@code writeResults(..)} which uses FileObjects instead
+     * of Strings.
+     * @param results
+     * @return
+     */
+    public int writeFOResults(Map<FileObject, String> results) {
+        final int[] r = {1};
+        results.forEach( (fo, c) -> {
+            r[0] &=
+                    writeResult(fo, c);
+        });
+
+        return r[0]-1;
+    }
+
+    public int writeResults(Map<String, String> results) {
+        final int[] r = {1};
+        results.forEach( (fo, c) -> {
+            r[0] &=
+                    writeResult(fo, c);
+        });
+
+        return r[0]-1;
+    }
+
+
+    private int writeStr(String str) {
         try {
             writer.write(str);
             return 0;
@@ -43,15 +109,6 @@ public class Writer {
         } catch (IOException e) {
             return -1;
         }
-    }
-
-    public static void main(String[] args) {
-        Writer w = new Writer("file.txt");
-        System.out.println(w.write("dnlk"));
-        System.out.println(w.write("\tkjrgnl"));
-        System.out.println(w.write("kjrgnl\n"));
-        System.out.println(w.write("kjrgnl\n"));
-        System.out.println(w.flush());
     }
 
 }
