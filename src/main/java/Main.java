50a6e3bf88e7a7361009a96164083fe002d28348
Eugene <ea4u16@soton.ac.uk>
2020-12-23 12:33:31 +0000

    First attempt at extraction and classification using Naives Bayes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -31,38 +31,40 @@ import java.util.Map;
 
 public class Main {
 
-//    public static void main(String[] args) throws IOException {
-//
-//        // have to specify a caltech record rather than the abstract record made by java
-//        VFSGroupDataset<FImage> trainingSet = new VFSGroupDataset<>("zip:training.zip", ImageUtilities.FIMAGE_READER);
-//
-//
-//        DenseSIFT dsift = new DenseSIFT(3, 7);
-//        PyramidDenseSIFT<FImage> pdsift = new PyramidDenseSIFT<>(dsift, 6f, 4,6,8,10);
-//
-//        // I think this works..? seemed to have worse accuracy however
-//        HardAssigner<byte[], float[], IntFloatPair> assigner;
-//        assigner = trainQuantiser(GroupedUniformRandomisedSampler.sample(trainingSet, 30), pdsift);
-//
-//        FeatureExtractor<DoubleFV, Caltech101.Record<FImage>> extractor = new PHOWExtractor(pdsift, assigner);
-//        // using homogenous kernal map... accuracy went up when using it
-//        HomogeneousKernelMap hkm = new HomogeneousKernelMap(HomogeneousKernelMap.KernelType.Chi2, HomogeneousKernelMap.WindowType.Rectangular);
-//        FeatureExtractor<DoubleFV, Caltech101.Record<FImage>> hkmExtractor = hkm.createWrappedExtractor(extractor);
-//
-//
-//        LiblinearAnnotator<Caltech101.Record<FImage>, String> ann = new LiblinearAnnotator<>(
-//                hkmExtractor, LiblinearAnnotator.Mode.MULTICLASS, SolverType.L2R_L2LOSS_SVC, 1.0, .00001);
-//        ann.train(splits.getTrainingDataset());
-//
-//        ClassificationEvaluator<CMResult<String>, String, Caltech101.Record<FImage>> eval =
-//                new ClassificationEvaluator<>(
-//                        ann, splits.getTestDataset(), new CMAnalyser<>(CMAnalyser.Strategy.SINGLE));
-//
-//        Map<Caltech101.Record<FImage>, ClassificationResult<String>> guesses = eval.evaluate();
-//        CMResult<String> result = eval.analyse(guesses);
-//
-//        // print results to sdtout
-//        System.out.println(result.getDetailReport());
-//    }
+    public static void main(String[] args) throws IOException {
+
+        /*// have to specify a caltech record rather than the abstract record made by java
+        VFSGroupDataset<FImage> trainingSet = new VFSGroupDataset<>("zip:training.zip", ImageUtilities.FIMAGE_READER);
+
+
+        DenseSIFT dsift = new DenseSIFT(3, 7);
+        PyramidDenseSIFT<FImage> pdsift = new PyramidDenseSIFT<>(dsift, 6f, 4,6,8,10);
+
+        // I think this works..? seemed to have worse accuracy however
+        HardAssigner<byte[], float[], IntFloatPair> assigner;
+        assigner = trainQuantiser(GroupedUniformRandomisedSampler.sample(trainingSet, 30), pdsift);
+
+        FeatureExtractor<DoubleFV, Caltech101.Record<FImage>> extractor = new PHOWExtractor(pdsift, assigner);
+        // using homogenous kernal map... accuracy went up when using it
+        HomogeneousKernelMap hkm = new HomogeneousKernelMap(HomogeneousKernelMap.KernelType.Chi2, HomogeneousKernelMap.WindowType.Rectangular);
+        FeatureExtractor<DoubleFV, Caltech101.Record<FImage>> hkmExtractor = hkm.createWrappedExtractor(extractor);
+
+
+        LiblinearAnnotator<Caltech101.Record<FImage>, String> ann = new LiblinearAnnotator<>(
+                hkmExtractor, LiblinearAnnotator.Mode.MULTICLASS, SolverType.L2R_L2LOSS_SVC, 1.0, .00001);
+        ann.train(splits.getTrainingDataset());
+
+        ClassificationEvaluator<CMResult<String>, String, Caltech101.Record<FImage>> eval =
+                new ClassificationEvaluator<>(
+                        ann, splits.getTestDataset(), new CMAnalyser<>(CMAnalyser.Strategy.SINGLE));
+
+        Map<Caltech101.Record<FImage>, ClassificationResult<String>> guesses = eval.evaluate();
+        CMResult<String> result = eval.analyse(guesses);
+
+        // print results to sdtout
+        System.out.println(result.getDetailReport());*/
+        new Run3Classifier();
+    }
+
 
 }
--- src/main/java/Run3Classifier.java
+++ src/main/java/Run3Classifier.java
@@ -1,9 +1,7 @@
 import de.bwaldvogel.liblinear.SolverType;
 import org.openimaj.data.DataSource;
-import org.openimaj.data.dataset.Dataset;
-import org.openimaj.data.dataset.GroupedDataset;
-import org.openimaj.data.dataset.ListDataset;
-import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.data.DataUtils;
+import org.openimaj.data.dataset.*;
 import org.openimaj.experiment.dataset.sampling.GroupSampler;
 import org.openimaj.experiment.dataset.sampling.GroupedUniformRandomisedSampler;
 import org.openimaj.experiment.dataset.split.GroupedRandomSplitter;
@@ -17,7 +15,9 @@ import org.openimaj.feature.FeatureExtractor;
 import org.openimaj.feature.SparseIntFV;
 import org.openimaj.feature.local.data.LocalFeatureListDataSource;
 import org.openimaj.feature.local.list.LocalFeatureList;
+import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
+import org.openimaj.image.Image;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.annotation.evaluation.datasets.Caltech101;
 import org.openimaj.image.feature.dense.gradient.dsift.ByteDSIFTKeypoint;
@@ -26,14 +26,13 @@ import org.openimaj.image.feature.dense.gradient.dsift.PyramidDenseSIFT;
 import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
 import org.openimaj.image.feature.local.aggregate.BlockSpatialAggregator;
 import org.openimaj.io.IOUtils;
-import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
+import org.openimaj.ml.annotation.bayes.NaiveBayesAnnotator;
+import org.openimaj.ml.annotation.svm.SVMAnnotator;
 import org.openimaj.ml.clustering.ByteCentroidsResult;
 import org.openimaj.ml.clustering.assignment.HardAssigner;
 import org.openimaj.ml.clustering.kmeans.ByteKMeans;
-import org.openimaj.ml.kernel.HomogeneousKernelMap;
 import org.openimaj.time.Timer;
 import org.openimaj.util.pair.IntFloatPair;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -43,20 +42,29 @@ import java.util.Map;
 public class Run3Classifier {
 
     //TODO: Edit this to remove or possibly implement
-    private static final String CACHE_PATH = "src/main/java/com/ea4u16/ch12/cache";
+    private static final String CACHE_PATH = "src/main/java/cache";
     private static final Timer timer = new Timer();
+
+    //TODO:
     static  {
         System.out.println("Running Classifier...");
         try {
-            //Grouped Dataset
-            GroupedDataset<String, VFSListDataset<Caltech101.Record<FImage>>, Caltech101.Record<FImage>> allData =
-                    Caltech101.getData(ImageUtilities.FIMAGE_READER);
 
-            GroupedDataset<String, ListDataset<Caltech101.Record<FImage>>, Caltech101.Record<FImage>> data =
-                    GroupSampler.sample(allData, 5, false);
+            VFSGroupDataset<FImage> trainImages =
+                    new VFSGroupDataset<>("/Users/eugeneagyeman/Documents/CSP3/Computer Vision/CW3/classification/training", ImageUtilities.FIMAGE_READER);
 
-            GroupedRandomSplitter<String, Caltech101.Record<FImage>> splits =
-                    new GroupedRandomSplitter<String, Caltech101.Record<FImage>>(data, 15, 0, 15);
+            VFSGroupDataset<FImage> testImages = new VFSGroupDataset<FImage>("/Users/eugeneagyeman/Documents/CSP3/Computer Vision/CW3/classification/testing",ImageUtilities.FIMAGE_READER);
+            //Debug to show images
+            /*for(final Map.Entry<String,VFSListDataset<FImage>> entry: trainImages.entrySet()) {
+                DisplayUtilities.display(entry.getKey(),entry.getValue());
+            }*/
+
+            /*
+            GroupedDataset<String, ListDataset<FImage>, FImage> data =
+                    GroupSampler.sample(images, 15, false);
+
+            GroupedRandomSplitter<String, FImage> splits =
+                    new GroupedRandomSplitter<String, FImage>(data, 15, 0, 15);*/
 
             //SIFTs
             DenseSIFT denseSIFT = new DenseSIFT(5, 7);
@@ -72,59 +80,54 @@ public class Run3Classifier {
                 System.out.println("Cache Loaded...");
             } catch (NullPointerException noFile) {
                 System.out.println("Cache Not Found, creating new one...");
-                assigner = trainQuantiser(GroupedUniformRandomisedSampler.sample(splits.getTrainingDataset(), 30), pyramidDenseSIFT);
+                assigner = trainQuantiser(GroupedUniformRandomisedSampler.sample(trainImages, 30), pyramidDenseSIFT);
                 cacheFile = new File(CACHE_PATH);
                 cacheFile.createNewFile();
                 IOUtils.writeToFile(assigner, cacheFile);
             } catch (IOException noFile) {
                 System.out.println("Error in accessing cache... creating new cache");
-                assigner = trainQuantiser(GroupedUniformRandomisedSampler.sample(splits.getTrainingDataset(), 30), pyramidDenseSIFT);
+                assigner = trainQuantiser(GroupedUniformRandomisedSampler.sample(trainImages, 30), pyramidDenseSIFT);
                 cacheFile = new File(CACHE_PATH);
                 cacheFile.createNewFile();
                 IOUtils.writeToFile(assigner, cacheFile);
             }
 
-            FeatureExtractor<DoubleFV, Caltech101.Record<FImage>> extractor = new PHOWExtractor(pyramidDenseSIFT, assigner);
-
-            //Exercise 1: Homogeneous Kernel Map
-            HomogeneousKernelMap homogeneousKernelMap = new HomogeneousKernelMap(HomogeneousKernelMap.KernelType.Chi2, HomogeneousKernelMap.WindowType.Rectangular);
-            FeatureExtractor<DoubleFV, Caltech101.Record<FImage>> homogeneousFeatureExtractor = homogeneousKernelMap.createWrappedExtractor(extractor);
+            FeatureExtractor<DoubleFV, FImage> extractor = new PHOWExtractor(pyramidDenseSIFT, assigner);
 
             //Exercise 2: Feature Caching
-            DiskCachingFeatureExtractor<DoubleFV, Caltech101.Record<FImage>> diskCachingFeatureExtractor = new DiskCachingFeatureExtractor<>(cacheFile, homogeneousFeatureExtractor);
-            LiblinearAnnotator<Caltech101.Record<FImage>, String> ann = new LiblinearAnnotator<>(
+            //DiskCachingFeatureExtractor<DoubleFV, FImage> diskCachingFeatureExtractor = new DiskCachingFeatureExtractor<>(cacheFile, homogeneousFeatureExtractor);
+
+            /*LiblinearAnnotator<Caltech101.Record<FImage>, String> ann = new LiblinearAnnotator<>(
                     diskCachingFeatureExtractor,
                     LiblinearAnnotator.Mode.MULTICLASS,
                     SolverType.L2R_L2LOSS_SVC,
                     1.0,
-                    0.00001);
+                    0.00001);*/
+
+            NaiveBayesAnnotator<FImage,String> svmann = new NaiveBayesAnnotator<FImage, String>(extractor, NaiveBayesAnnotator.Mode.ALL);
 
             timer.start();
-            ann.train(splits.getTrainingDataset());
+            svmann.train(trainImages);
             timer.stop();
 
             long resultantTime = timer.duration();
             System.out.println("Time: " + resultantTime);
 
-            ClassificationEvaluator<CMResult<String>, String, Caltech101.Record<FImage>> eval =
-                    new ClassificationEvaluator<CMResult<String>, String, Caltech101.Record<FImage>>(
-                            ann,
-                            splits.getTestDataset(),
-                            new CMAnalyser<Caltech101.Record<FImage>, String>(CMAnalyser.Strategy.SINGLE));
-            Map<Caltech101.Record<FImage>, ClassificationResult<String>> guesses = eval.evaluate();
+            ClassificationEvaluator<CMResult<String>, String, FImage> eval = new ClassificationEvaluator<>(svmann,testImages,new CMAnalyser<>(CMAnalyser.Strategy.SINGLE));
+
+            Map<FImage, ClassificationResult<String>> guesses = eval.evaluate();
             CMResult<String> result = eval.analyse(guesses);
             result.getDetailReport();
-            System.out.println("CH12 Completed.");
+            System.out.println("Classification Completed.");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    static HardAssigner<byte[], float[], IntFloatPair> trainQuantiser(Dataset<Caltech101.Record<FImage>> sample, PyramidDenseSIFT<FImage> pyramidDenseSIFT) {
+    static HardAssigner<byte[], float[], IntFloatPair> trainQuantiser(Dataset<FImage> sample, PyramidDenseSIFT<FImage> pyramidDenseSIFT) {
         List<LocalFeatureList<ByteDSIFTKeypoint>> allkeys = new
-                ArrayList<LocalFeatureList<ByteDSIFTKeypoint>>();
-        for (Caltech101.Record<FImage> rec : sample) {
-            FImage img = rec.getImage();
+                ArrayList<>();
+        for (FImage img : sample) {
             pyramidDenseSIFT.analyseImage(img);
             allkeys.add(pyramidDenseSIFT.getByteKeypoints(0.005f));
         }
@@ -134,11 +137,21 @@ public class Run3Classifier {
         DataSource<byte[]> datasource = new LocalFeatureListDataSource<ByteDSIFTKeypoint, byte[]>(allkeys);
         ByteCentroidsResult result = km.cluster(datasource);
         return result.defaultHardAssigner();
-
-
     }
+    class Record<FImage> {
+        private String imageClass;
+        private FImage image;
 
-    static class PHOWExtractor implements FeatureExtractor<DoubleFV, Caltech101.Record<FImage>> {
+        public Record(FImage image) {
+        }
+        FImage getImage() {
+            return image;
+        }
+        String getImageClass() {
+            return imageClass;
+        }
+    }
+    static class PHOWExtractor implements FeatureExtractor<DoubleFV, FImage> {
         PyramidDenseSIFT<FImage> pdsift;
         HardAssigner<byte[], float[], IntFloatPair> assigner;
 
@@ -148,13 +161,13 @@ public class Run3Classifier {
             this.assigner = assigner;
         }
 
-        public DoubleFV extractFeature(Caltech101.Record<FImage> object) {
-            FImage image = object.getImage();
-            pdsift.analyseImage(image);
+        public DoubleFV extractFeature(FImage fImage) {
+
+            pdsift.analyseImage(fImage);
             BagOfVisualWords<byte[]> bovw = new BagOfVisualWords<>(assigner);
             BlockSpatialAggregator<byte[], SparseIntFV> spatial = new BlockSpatialAggregator<>(
                     bovw, 2, 2);
-            return spatial.aggregate(pdsift.getByteKeypoints(0.015f), image.getBounds()).normaliseFV();
+            return spatial.aggregate(pdsift.getByteKeypoints(0.015f), fImage.getBounds()).normaliseFV();
         }
     }
 }
